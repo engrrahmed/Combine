dd6f8990c5834a3b63a143e43cb2706ff1032d26
Rizwan Ahmed <rizwan.ahmed@tintash.com>
2019-07-31 18:36:47 +0500

    Adding Button and Text Field in the Project for the Demo

--- CombineTestProject/CombineTestProject/ViewController.swift
+++ CombineTestProject/CombineTestProject/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField    : UITextField!
+    @IBOutlet weak var confirmTextField     : UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickOnSignUpButton(_ sender: Any) {
+        
+    }
+    
 }
 
