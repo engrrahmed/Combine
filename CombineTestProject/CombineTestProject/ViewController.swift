3c8d24a93b484ed1ce53d67438549cdcc309a4b5
Rizwan Ahmed <rizwan.ahmed@tintash.com>
2019-08-01 12:15:26 +0500

    Added Sink Subscriber on text field to show data

--- CombineTestProject/CombineTestProject/ViewController.swift
+++ CombineTestProject/CombineTestProject/ViewController.swift
@@ -7,20 +7,48 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var showTextLabel        : UILabel!
     @IBOutlet weak var passwordTextField    : UITextField!
     @IBOutlet weak var confirmTextField     : UITextField!
+    @IBOutlet weak var signUpButton         : UIButton!
+    
+    @Published var passwordText         : String = ""
+    @Published var confirmPasswordText  : String = ""
+    
+    private var passwordSubscription: AnyCancellable?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func clickOnSignUpButton(_ sender: Any) {
         
+        passwordSubscription = $passwordText.sink {
+            print("The published value is for password '\($0)'")
+            self.showTextLabel.text = $0
+        }
+        
+        //        $passwordText.receive(on: DispatchQueue.main).assign(to: \.isEnabled, on: signUpButton)
+    }
+    
+    @IBAction func clickOnSignUpButton(_ sender: Any) {
+        passwordText = "ABC"
+        confirmPasswordText = "DGH"
+    }
+    
+    @IBAction func didUpdatingPassword(_ sender: UITextField) {
+        passwordText = sender.text ?? ""
+    }
+    
+    @IBAction func setUserPassword(_ sender: UITextField) {
+        passwordText = sender.text ?? ""
+    }
+    
+    @IBAction func setConfirmPassword(_ sender: UITextField) {
+        confirmPasswordText = sender.text ?? ""
     }
     
 }
-
