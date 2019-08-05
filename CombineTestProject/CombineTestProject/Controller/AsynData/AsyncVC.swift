7a628f96efe1128e5d7e6dfd99c7287398a4e777
Rizwan Ahmed <rizwan.ahmed@tintash.com>
2019-08-05 16:34:11 +0500

    Refactor Async VC class
    What: Split methods and add mark for class category
    Why:  refactoring class for readability
    Impact: low, it will not affect other part of the code

--- CombineTestProject/CombineTestProject/Controller/AsynData/AsyncVC.swift
+++ CombineTestProject/CombineTestProject/Controller/AsynData/AsyncVC.swift
@@ -10,84 +10,26 @@ import UIKit
 import Combine
 class AsyncVC: UIViewController {
     
-    
-    @IBOutlet weak var user1UpdateLabel    : UILabel!
-    @IBOutlet weak var user2UpdateLabel    : UILabel!
-    @IBOutlet weak var user3UpdateLabel    : UILabel!
-    @IBOutlet weak var user4UpdateLabel    : UILabel!
+    // MARK: - IBOutlet
+    @IBOutlet weak var user1UpdateLabel     : UILabel!
+    @IBOutlet weak var user2UpdateLabel     : UILabel!
+    @IBOutlet weak var user3UpdateLabel     : UILabel!
+    @IBOutlet weak var user4UpdateLabel     : UILabel!
     
     @IBOutlet weak var signUpButton         : UIButton!
     
+    // MARK: - viewmodel
     var viewModel = SignUpViewModel()
     
-    @Published var userName1             : String = ""
-    @Published var userName2             : String = ""
-    @Published var userName3             : String = ""
-    @Published var userName4             : String = ""
+    // MARK: - Published properties
+    @Published var userName1                : String = ""
+    @Published var userName2                : String = ""
+    @Published var userName3                : String = ""
+    @Published var userName4                : String = ""
     
-    private var cancellableSet          : Set<AnyCancellable> = []
+    private var cancellableSet              : Set<AnyCancellable> = []
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        signUpButton.setTitleColor(.red, for: .disabled)
-        signUpButton.setTitleColor(.white, for: .normal)
-        signUpButton.isEnabled = false
-        createPublisherForTextField(textField: UITextField(), statusLabel: user1UpdateLabel, username: $userName1)
-            .flatMap {  flatMapInValue -> AnyPublisher<Bool, Never> in
-                let pub2 = self.createPublisherForTextField(textField: UITextField(), statusLabel: self.user2UpdateLabel, username: self.$userName2)
-                let pub3 = self.createPublisherForTextField(textField: UITextField(), statusLabel: self.user3UpdateLabel, username: self.$userName3)
-                let pub4 = self.createPublisherForTextField(textField: UITextField(), statusLabel: self.user4UpdateLabel, username: self.$userName4)
-                return Publishers.Zip3(pub2, pub3, pub4)
-                    //                    return Publishers.CombineLatest3(pub2, pub3, pub4)
-                    .map{ (result1, result2, result3) ->Bool in
-                        return result1 && result2 && result3
-                }.eraseToAnyPublisher()
-        }//.eraseToAnyPublisher()
-            
-            
-            //        self.readyToSubmit
-            //            .map { $0 != nil }
-            .receive(on: RunLoop.main)
-            ////            .replaceError(with: false)
-            //            .sink { (valid) in
-            //                print("CombineLatest: Are the credentials valid: \(valid)")
-            //        }
-            .assign(to: \.isEnabled, on: signUpButton)
-            .store(in: &cancellableSet)
-    }
-    
-    func  createPublisherForTextField(textField :UITextField, statusLabel: UILabel, username: Published<String>.Publisher ) -> AnyPublisher<Bool, Never> {
-        //        var validatedUsername: AnyPublisher<String?, Never> {
-        //            return
-        return username
-            .debounce(for: 0.5, scheduler: RunLoop.main)
-            .removeDuplicates()
-            .filter { stringValue  in return stringValue != ""}
-            .flatMap { userName   in
-                return Future { promise in //(Result<Output, Failure>) -> Void
-                    self.viewModel.findUserIDFor(userId: userName) { available in
-                        //                            self.userNotFoundLabel.isHidden = available
-                        promise(.success(available ? userName : nil))
-                    }
-                }
-        }.receive(on: RunLoop.main)
-            // so that we can update UI elements to show the "completion"
-            // of this step
-            .map { inValue -> Bool in
-                // intentially side effecting here to show progress of pipeline
-                //                seld
-                self.updateLable(label: statusLabel, isValid: (inValue != nil))
-                return (inValue != nil)
-        }
-        .eraseToAnyPublisher()
-        //        }
-    }
-    func updateLable(label: UILabel, isValid: Bool) {
-        label.text = (isValid) ? "User Found" : "User is not Found"
-        label.textColor = (isValid) ? .green : .red
-    }
+    // MARK: - IBAction
     @IBAction func clickOnSignUpButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -104,4 +46,67 @@ class AsyncVC: UIViewController {
     @IBAction func userName4Changed(_ sender: UITextField) {
         userName4 = sender.text ?? ""
     }
+    
+    // MARK: - View controller methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        signUpButton.setTitleColor(.red, for: .disabled)
+        signUpButton.setTitleColor(.white, for: .normal)
+        signUpButton.isEnabled = false
+        setupAsyncPublishers()
+    }
+}
+
+// MARK: - Class methods
+extension AsyncVC {
+    func updateLable(label: UILabel, isValid: Bool) {
+        label.text = (isValid) ? "User Found" : "User is not Found"
+        label.textColor = (isValid) ? .green : .red
+    }
+}
+
+// MARK: - Combine methods
+extension AsyncVC {
+    func setupAsyncPublishers() {
+        createPublisherForTextField(username: $userName1, statusLabel: user1UpdateLabel)
+            .flatMap {  flatMapInValue -> AnyPublisher<Bool, Never> in
+                let pub2 = self.createPublisherForTextField(username: self.$userName2, statusLabel: self.user2UpdateLabel)
+                let pub3 = self.createPublisherForTextField(username: self.$userName3, statusLabel: self.user3UpdateLabel)
+                let pub4 = self.createPublisherForTextField(username: self.$userName4, statusLabel: self.user4UpdateLabel)
+                return Publishers.Zip3(pub2, pub3, pub4)
+                    //                    return Publishers.CombineLatest3(pub2, pub3, pub4)
+                    .map{ (result1, result2, result3) ->Bool in
+                        return result1 && result2 && result3
+                }.eraseToAnyPublisher()
+        }
+        .receive(on: RunLoop.main)
+            //            .replaceError(with: false) // If there is Publisher type is <Bool, Error> instead of <Bool, Never>
+            //            .sink { (valid) in
+            //                print("CombineLatest: Are the credentials valid: \(valid)")
+            //        }
+            .assign(to: \.isEnabled, on: signUpButton)
+            .store(in: &cancellableSet)
+    }
+    
+    func  createPublisherForTextField(username: Published<String>.Publisher, statusLabel: UILabel,textField :UITextField? = nil ) -> AnyPublisher<Bool, Never> {
+        return username
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .filter { stringValue  in return stringValue != ""}
+            .flatMap { userName   in
+                return Future { promise in //(Result<Output, Failure>) -> Void
+                    self.viewModel.findUserIDFor(userId: userName) { available in
+                        promise(.success(available ? userName : nil))
+                    }
+                }
+        }.receive(on: RunLoop.main)
+            .map { inValue -> Bool in
+                // Update UI respective element for the call
+                self.updateLable(label: statusLabel, isValid: (inValue != nil))
+                return (inValue != nil)
+        }
+        .eraseToAnyPublisher()
+    }
+    
 }
